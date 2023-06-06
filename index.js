a989df4c2c18fcdffa40fccd1262b7ec1e912217
Mikhail Yarmaliuk <hellboymxb@gmail.com>
2023-06-07 00:38:14 +0200

    fix: patch version with semantic release

--- index.js
+++ index.js
@@ -330,14 +330,33 @@ const runSemanticRelease = (isDryRun = false) => {
     childProcess.execSync(`npx semantic-release ${isDryRun ? '--dryRun' : ''}`, {
       stdio: 'inherit',
       cwd: msDir,
-      env: {
-        ...process.env,
-        SEMANTIC_WORKING_DIR: msDir,
-        SEMANTIC_ROOT_DIR: rootDir,
-      },
+      env: { ...process.env },
     });
 
-    console.info(chalk.green(`Semantic release done: ${msDir}`));
+    let nextVersion = 'unknown';
+
+    try {
+      const versionFile = findFile(['.version'], `${rootDir}/${msDir}`);
+
+      if (!versionFile) {
+        throw new Error("Package file version doesn't exist.");
+      }
+
+      nextVersion = (fs.readFileSync(versionFile, { encoding: 'utf-8' }) || '').trim();
+
+      if (!nextVersion) {
+        throw new Error('Package version is empty.');
+      }
+
+      childProcess.execSync(`npx @lomray/microservices-cli patch-package-version --package-version ${nextVersion} --dir ${msDir}`, {
+        stdio: 'inherit',
+        env: { ...process.env },
+      });
+    } catch (e) {
+      console.info(chalk.yellow(`Failed get package version: ${e.message}`));
+    }
+
+    console.info(chalk.green(`Semantic release done: ${msDir}. Version: ${nextVersion}`));
   }
 };
 
